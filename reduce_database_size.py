c94ba1055a7959916af4977abb217daf1436dd7f
Borja Muñoz <borja.munoz@gmail.com>
2024-04-18 00:41:20 +0200

    Update database file name and remove spatial extension

--- reduce_database_size.py
+++ reduce_database_size.py
@@ -7,25 +7,21 @@ from datetime import datetime
 now = datetime.now()
 dt_string = now.strftime("%Y%m%d_%H%M%S")
 print("Creating a database backup...")
-shutil.copy('db/reservoir-analysis.db', 'db/backup/reservoir-analysis_' + dt_string + '.db')
+shutil.copy('db/saih-explorer.db', 'db/backup/saih-explorer_' + dt_string + '.db')
 
 # Export database
 print("Exporting a database dump...")
-con = duckdb.connect('db/reservoir-analysis.db') # has big file size
+con = duckdb.connect('db/saih-explorer.db') # has big file size
 con.execute("EXPORT DATABASE 'db/db-dump';")
 con.close()
 
 # Remove database
 print("Removing existing database...")
-os.remove('db/reservoir-analysis.db')
+os.remove('db/saih-explorer.db')
 
 # Recreate database
-# In order to be able to load the spatial extension, it needs
-# to be pre-installed in the ~/.duckdb/extensions directory
-print("Creating a new database and loading the spatial extension...")
-con = duckdb.connect('db/reservoir-analysis.db')
-con.execute("INSTALL spatial;")
-con.execute("LOAD spatial;")
+print("Creating a new database...")
+con = duckdb.connect('db/saih-explorer.db')
 
 # Import data from the dump
 print("Importing from the dump files...")
